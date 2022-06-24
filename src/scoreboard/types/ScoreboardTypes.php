4cf467604985136e93a402ab9e439b45fdb2faec
Crayder <v5agarwal@ucsd.edu>
2022-06-24 12:43:32 +0530

    ScoreboardTypes: Update footer and Header

--- src/scoreboard/types/ScoreboardTypes.php
+++ src/scoreboard/types/ScoreboardTypes.php
@@ -8,12 +8,12 @@ use Crayder\Core\scoreboard\ScoreboardEntry;
 class ScoreboardTypes{
 
 	public static function main() : Scoreboard{
-		$scoreboard = new Scoreboard("§6§lKITPVP §r§c[Beta]");
+		$scoreboard = new Scoreboard("§6§lKITPVP §r§c(Beta)");
 
 		$scoreboard->addEntry(new ScoreboardEntry(0, " "));
 
 		$scoreboard->addEntry(new ScoreboardEntry(10, "  "));
-		$scoreboard->addEntry(new ScoreboardEntry(11, "stcraftnet.com  "));
+		$scoreboard->addEntry(new ScoreboardEntry(11, " » STCraftNet.com  "));
 
 		return $scoreboard;
 	}
@@ -25,7 +25,7 @@ class ScoreboardTypes{
 		$scoreboard->addEntry(new ScoreboardEntry(1, " §eStaff Mode: §6Enabled "));
 		$scoreboard->addEntry(new ScoreboardEntry(2, "  "));
 		$scoreboard->addEntry(new ScoreboardEntry(6, "  "));
-		$scoreboard->addEntry(new ScoreboardEntry(7, "stcraftnet.com  "));
+		$scoreboard->addEntry(new ScoreboardEntry(7, " » STCraftNet.com  "));
 
 		return $scoreboard;
 	}
